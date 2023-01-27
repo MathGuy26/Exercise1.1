c6c6976937d7d0cbe9738f0ec47d9b9c03810b1b
Brandon <6256162+BevansMath@users.noreply.github.com>
2023-01-27 03:56:45 -0600

    Feature changes; Included URL as command line argument Included integer Exception; command line arguments should only be able to accept a string as input

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,35 @@
 import java.net.*;
 import java.io.*;
 class DownloadFileFromUrl {
-    public static void main(String[] args){ // entry point to main program, exception is an unreachable URL
+    public static void main(String[] args){
+        if(args.length < 2){
+            System.out.println("Usage: Java DownloadFileFromUrl <URL> <inputWord>");
+            return;
+        }
+        String url = args[0];
+        String inputWord = args[1];
         try {
-            URL data = new URL("https://gist.githubusercontent.com/Thiamath/b84075e5b1c6be151b444e14b4a88be6/raw/dc67a96c38722523574134acf24848f3f6a1c40f/lipsum.txt");
+            URL data = new URL(url);
             BufferedReader in = new BufferedReader(new InputStreamReader(data.openStream()));
-
-            String inputWord;
-            while((inputWord = in.readLine()) != null) {
-                System.out.println(inputWord);
+            String inputLine;
+            int wordCount = 0;
+            while((inputLine = in.readLine()) != null) {
+                String[] wordsInLine = inputLine.split(" ");
+                for (String word : wordsInLine) {
+                    if(word.equalsIgnoreCase(inputWord)) {
+                        wordCount++;
+                    }
+                }
             }
             in.close();
+            System.out.println("The word " + inputWord + " appears " + wordCount + " time in the file. ");
 
         } catch (MalformedURLException e) {
             System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("I/O error: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid argument, please provide a string value as the word to search");
         }
 
 
